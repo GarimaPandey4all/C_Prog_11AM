269c36232d2d58a0b4f0890ef037476612f5a6d6
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-10-26 11:06:47 +0530

    Add files via upload

---
+++ VIVEKH.C
@@ -0,0 +1,8 @@
+#include<stdio.h>    // stdio.h : standard input / output
+
+void main()   // entry point
+{
+	clrscr(); // clear screen
+	printf("Hello World"); // print output on the output screen
+	getch(); // get character
+}
